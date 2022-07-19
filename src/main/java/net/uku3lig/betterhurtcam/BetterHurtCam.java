d3e439d3c12c2aec9d809554c7cd9213b0809b84
uku <uku3lig@gmail.com>
2022-07-20 00:09:33 +0200

    remove useless import

--- src/main/java/net/uku3lig/betterhurtcam/BetterHurtCam.java
+++ src/main/java/net/uku3lig/betterhurtcam/BetterHurtCam.java
@@ -8,7 +8,6 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.LiteralTextContent;
 import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
