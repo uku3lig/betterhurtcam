afa2a3ac3ebd4f4342cd07558944cd81d332c586
uku <uku3lig@gmail.com>
2022-09-02 19:54:25 +0200

    fix slider in config screen

--- src/main/java/net/uku3lig/betterhurtcam/config/ConfigScreen.java
+++ src/main/java/net/uku3lig/betterhurtcam/config/ConfigScreen.java
@@ -1,5 +1,6 @@
 package net.uku3lig.betterhurtcam.config;
 
+import com.mojang.serialization.Codec;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.option.SimpleOption;
@@ -16,7 +17,8 @@ public class ConfigScreen extends AbstractConfigScreen {
                 SimpleOption.ofBoolean("betterhurtcam.option.enabled", config.isEnabled(), config::setEnabled),
                 new SimpleOption<>("betterhurtcam.option.strength", SimpleOption.emptyTooltip(),
                         (text, value) -> GameOptions.getGenericValueText(text, Text.of(value.toString())),
-                        SimpleOption.DoubleSliderCallbacks.INSTANCE, config.getMultiplier(), config::setMultiplier)
+                        new SimpleOption.ValidatingIntSliderCallbacks(0, 20).withModifier(value -> value / 10., value -> (int) (value * 10)),
+                        Codec.doubleRange(0, 2), config.getMultiplier(), config::setMultiplier)
         };
     }
 }
