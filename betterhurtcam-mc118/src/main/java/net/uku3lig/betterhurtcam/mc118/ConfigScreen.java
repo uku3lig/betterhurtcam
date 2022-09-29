6e69580f3bcaecd6fda87444f6d48fcafe897ab4
uku <uku3lig@gmail.com>
2022-09-29 12:01:52 +0200

    add 1.18 module

---
+++ betterhurtcam-mc118/src/main/java/net/uku3lig/betterhurtcam/mc118/ConfigScreen.java
@@ -0,0 +1,26 @@
+package net.uku3lig.betterhurtcam.mc118;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.option.CyclingOption;
+import net.minecraft.client.option.Option;
+import net.minecraft.text.Text;
+import net.uku3lig.betterhurtcam.config.BHCConfig;
+import net.uku3lig.betterhurtcam.config.MultiplierInputScreen;
+import net.uku3lig.ukulib.config.ConfigManager;
+import net.uku3lig.ukulib.config.screen.AbstractConfigScreen;
+import net.uku3lig.ukulib.utils.Ukutils;
+
+public class ConfigScreen extends AbstractConfigScreen<BHCConfig> {
+    public ConfigScreen(Screen parent, ConfigManager<BHCConfig> manager) {
+        super(parent, Text.of("BetterHurtCam Config"), manager);
+    }
+
+    @Override
+    @SuppressWarnings("ConstantConditions")
+    protected Option[] getOptions(BHCConfig config) {
+        return new Option[] {
+                CyclingOption.create("betterhurtcam.option.enabled", opt -> config.isEnabled(), (opt, option, value) -> config.setEnabled(value)),
+                Ukutils.createButton("betterhurtcam.option.strength", config.getMultiplier(), screen -> client.setScreen(new MultiplierInputScreen(screen, manager)))
+        };
+    }
+}
