12eaca08a56a7fbe956a1a51981d2424205272f2
uku <uku3lig@gmail.com>
2022-11-25 00:25:13 +0100

    port to 1.19.3-pre2

---
+++ betterhurtcam-mc1193/src/main/java/net/uku3lig/betterhurtcam/mc1193/UkulibIntegration.java
@@ -0,0 +1,15 @@
+package net.uku3lig.betterhurtcam.mc1193;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.uku3lig.betterhurtcam.BetterHurtCam;
+import net.uku3lig.ukulib.api.UkulibAPI;
+import net.uku3lig.ukulib.config.screen.AbstractConfigScreen;
+
+import java.util.function.Function;
+
+public class UkulibIntegration implements UkulibAPI {
+    @Override
+    public Function<Screen, AbstractConfigScreen<?>> supplyConfigScreen() {
+        return parent -> new ConfigScreen(parent, BetterHurtCam.getManager());
+    }
+}
