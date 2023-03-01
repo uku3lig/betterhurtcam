f47b520dd71a6c3ed52f5bb46cb8e8c3fccb9b32
uku <uku3lig@gmail.com>
2023-03-01 12:00:43 +0100

    update to 1.19.4-pre2

---
+++ betterhurtcam-mc1194/src/main/java/net/uku3lig/betterhurtcam/mc1194/ConfigScreen.java
@@ -0,0 +1,26 @@
+package net.uku3lig.betterhurtcam.mc1194;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.option.SimpleOption;
+import net.minecraft.text.Text;
+import net.uku3lig.betterhurtcam.BetterHurtCam;
+import net.uku3lig.betterhurtcam.config.BHCConfig;
+import net.uku3lig.ukulib.config.ConfigManager;
+import net.uku3lig.ukulib.config.screen.AbstractConfigScreen;
+import net.uku3lig.ukulib.utils.Ukutils;
+
+public class ConfigScreen extends AbstractConfigScreen<BHCConfig> {
+    public ConfigScreen(Screen parent, ConfigManager<BHCConfig> manager) {
+        super(parent, Text.of("BetterHurtCam Config"), manager);
+    }
+
+    @Override
+    protected SimpleOption<?>[] getOptions(BHCConfig config) {
+        return new SimpleOption[] {
+                SimpleOption.ofBoolean("betterhurtcam.option.enabled", config.isEnabled(), config::setEnabled),
+                Ukutils.createOpenButton("betterhurtcam.option.strength", BetterHurtCam.format(config.getMultiplier()),
+                        parent -> new MultiplierInputScreen(parent, manager)),
+                SimpleOption.ofBoolean("betterhurtcam.option.heartBlink", config.isHeartBlink(), config::setHeartBlink)
+        };
+    }
+}
