6a2ac06d73a35bde38ad74d83eeb6009b52b7aa9
uku <uku3lig@gmail.com>
2023-06-07 17:40:34 +0200

    1.20

--- betterhurtcam-mc120/src/main/java/net/uku3lig/betterhurtcam/mc120/mixin/MixinInGameHud.java
+++ betterhurtcam-mc120/src/main/java/net/uku3lig/betterhurtcam/mc120/mixin/MixinInGameHud.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(InGameHud.class)
 public class MixinInGameHud {
-    @ModifyArg(method = "renderStatusBars", index = 10, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHealthBar(Lnet/minecraft/client/gui/DrawableHelper;Lnet/minecraft/entity/player/PlayerEntity;IIIIFIIIZ)V"))
+    @ModifyArg(method = "renderStatusBars", index = 10, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHealthBar(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/entity/player/PlayerEntity;IIIIFIIIZ)V"))
     public boolean renderHearts(boolean blinking) {
         return blinking && BetterHurtCam.getManager().getConfig().isHeartBlink();
     }
